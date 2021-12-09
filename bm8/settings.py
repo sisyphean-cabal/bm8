a9b16b7e34221dbaff7999be10a53b0de61ab836
Preston <preston@sudogami.com>
2021-12-09 15:15:53 -0500

    first migrations, postgres setup, init complete

--- bm8/settings.py
+++ bm8/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'bm8.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'bmtbl',
+        'USER': 'postgres',
+        'PASSWORD': 'bm152612',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
