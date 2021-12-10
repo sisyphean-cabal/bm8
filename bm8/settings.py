ad55ad5cfac3b8a7e6c138f8cc1979566689d0a7
Preston <preston@sudogami.com>
2021-12-10 16:29:49 -0500

    webpack works, react broken"

--- bm8/settings.py
+++ bm8/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts.apps.AccountsConfig',
+    'frontend.apps.FrontendConfig',
     'rest_framework',
     'corsheaders'
 ]
