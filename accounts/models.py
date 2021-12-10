10267e1484a4bf6c125afeb32d107fa4af1fdfce
Preston <preston@sudogami.com>
2021-12-10 09:56:40 -0500

    models for bands, genres, album, profile, in accounts app

--- accounts/models.py
+++ accounts/models.py
@@ -1,1 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Album(models.Model):
+    name = models.CharField(max_length=500)
+
+
+class Bands(models.Model):
+    name = models.CharField(max_length=500)
+    albums = models.ManyToManyField(Album)
+
+
+class Genres(models.Model):
+    name = models.CharField(max_length=90)
+    bands = models.ManyToManyField(Bands)
+    albums = models.ManyToManyField(Album)
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    genres = models.ManyToManyField(Genres)
