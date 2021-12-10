6bd3fe0504677aefcd84e2d82b696bfffcdcd688
Preston <preston@sudogami.com>
2021-12-10 09:22:55 -0500

    requirements.txt, register.html updates, settings updates, djangorestframework

--- bm8/settings.py
+++ bm8/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'bm8.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [BASE_DIR / 'templates', BASE_DIR / 'accounts/templates/']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
