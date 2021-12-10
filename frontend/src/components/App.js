b49c90c628e160072c8669affd6894767e562f64
Preston <preston@sudogami.com>
2021-12-10 16:48:02 -0500

    components render

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
 import ReactDOM from 'react-dom'
+
+import HomePage from "./HomePage"
+
 export default class App extends Component {
     constructor(props){
         super(props)
     }
 
     render(){
-        return<h1>Testing Testing</h1>;
+        return <HomePage/>;
     }
 }
 
