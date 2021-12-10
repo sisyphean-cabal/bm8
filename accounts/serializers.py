98397ff03c221df32015035a3d98ba70ffffd72e
Preston <preston@sudogami.com>
2021-12-10 10:33:50 -0500

    ProfileSerializer updated due to misconfig

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -5,5 +5,8 @@ from .models import User, Album, Band, Genre
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('user', 'genres', 'albums', 'bands')
+        fields = ('username', 'first_name', 'last_name',
+                  'email', 'password', 'groups',
+                  'user_permissions', 'is_staff', 'is_active',
+                  'is_superuser', 'last_login', 'date_joined')
 
