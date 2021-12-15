9e70551201b1636c1891d90ac55652b9b0abae53
Preston <preston@sudogami.com>
2021-12-15 10:19:54 -0500

    moved into new api folder within accounts

---
+++ accounts/api/views.py
@@ -0,0 +1,6 @@
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+from accounts.api.serializers import RegistrationSerializer
+
--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,9 @@
-from django.db.models.query import QuerySet
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 from .models import Profile
-from .serializers import ProfileSerializer
-from accounts import serializers
+from .api.serializers import ProfileSerializer
 # Create your views here.
 
 
