53810a5c5849f020bf933eab41fa160395e70aa9
Preston <preston@sudogami.com>
2021-12-15 10:23:46 -0500

    serializers, registration

--- accounts/api/views.py
+++ accounts/api/views.py
@@ -1,6 +1,18 @@
 from rest_framework import generics
+from rest_framework import serializers
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework.serializers import Serializer
 
 from accounts.api.serializers import RegistrationSerializer
 
+@api_view(['POST',])
+def registration_View(req):
+    if req.method == 'POST':
+        serialzer = RegistrationSerializer(data=req.data)
+        data = {}
+        if serialzer.is_valid():
+            account = serialzer.save()
+            data['response'] = "registration successful"
+            data['email'] = account.email
+            data['username'] = account.username
