19399e591cefccd8c7976b69f05da1e3a78bbc52
Preston <preston@sudogami.com>
2021-12-15 10:16:33 -0500

    registration save to db

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -24,6 +24,14 @@ class RegistrationSerializer(serializers.ModelSerializer):
         account = User(
             email=self.validated_data['email'],
             username=self.validated_data['username']
-            
         )
+        password = self.validated_data['password']
+        password2 = self.validated_data['password2']
+
+        if password != password2:
+            raise serializers.ValidationError({'password': 'Passwords must match'})
+        account.set_password(password)
+        account.save()
+        return account
+
     
