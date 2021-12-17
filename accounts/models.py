52c114d0b6e8ae90aa121ed2f740e5dd4a5de4e5
Preston <preston@sudogami.com>
2021-12-16 23:37:45 -0500

    phone numbers

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.db.models import constraints
+from phonenumber_field.modelfields import PhoneNumberField
 
 class Album(models.Model):
     name = models.CharField(max_length=500)
@@ -20,6 +21,13 @@ class Profile(models.Model):
     user_obj = models.OneToOneField(User, on_delete=models.CASCADE)
     biography = models.CharField(max_length=1500)
     soundcloud = models.CharField(max_length=280)
-    genres = models.ManyToManyField(Genre)
-    albums = models.ManyToManyField(Album)
-    bands = models.ManyToManyField(Band)
+    phone_number = PhoneNumberField(blank=True)
+
+    # class Meta:
+    #     constraints = [
+    #         models.CheckConstraint(
+    #             name="%(app_label)s_%(class)s_username_phonenumber",
+    #             check=(models.phone)
+    #         )
+    #     ]
+
