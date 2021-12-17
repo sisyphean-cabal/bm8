049bed0402e7cd71113719f3c2d8d5fd0514699e
Preston <preston@sudogami.com>
2021-12-16 21:57:05 -0500

    camel case parser with rest framework

--- bm8/settings.py
+++ bm8/settings.py
@@ -53,6 +53,24 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+REST_FRAMEWORK = {
+
+    'DEFAULT_RENDERER_CLASSES': (
+        'djangorestframework_camel_case.render.CamelCaseJSONRenderer',
+        'djangorestframework_camel_case.render.CamelCaseBrowsableAPIRenderer',
+        # Any other renders
+    ),
+
+    'DEFAULT_PARSER_CLASSES': (
+        # If you use MultiPartFormParser or FormParser, we also have a camel case version
+        'djangorestframework_camel_case.parser.CamelCaseFormParser',
+        'djangorestframework_camel_case.parser.CamelCaseMultiPartParser',
+        'djangorestframework_camel_case.parser.CamelCaseJSONParser',
+        # Any other parsers
+    ),
+}
+
+
 ROOT_URLCONF = 'bm8.urls'
 
 TEMPLATES = [
--- common/camelSubjugator.py
+++
@@ -1,3 +0,0 @@
-def camel_sub(name):
-    components = name.split('_')
-    return print(components[0] + ''.join(x.title() for x in components[1:]))
