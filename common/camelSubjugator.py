ead1b11757cdcde92d0e521a20d274cd518d7fe9
Preston <preston@sudogami.com>
2021-12-16 21:49:49 -0500

    snake to camel

--- common/camelSubjugator.py
+++ common/camelSubjugator.py
@@ -1,7 +1,3 @@
-def test_camel_sub(name):
+def camel_sub(name):
     components = name.split('_')
     return print(components[0] + ''.join(x.title() for x in components[1:]))
-
-
-test_camel_sub("penis_monsta")
-
