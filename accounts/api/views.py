e74bc4cb6cf77786561e7583185af87db4aa1e50
Preston <preston@sudogami.com>
2021-12-26 20:17:40 -0500

    ObtainAuthToken class

--- accounts/api/views.py
+++ accounts/api/views.py
@@ -1,8 +1,26 @@
-from rest_framework import generics
-from rest_framework.response import Response
+from rest_framework import generics, parsers, renderers
 from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 from accounts.api.serializers import RegistrationSerializer, UserSerializer
 from accounts.models import User
+
+
+class ObtainAuthToken(APIView):
+    throttle_classes = ()
+    permission_classes = ()
+    parser_classes = (
+        parsers.FormParser,
+        parsers.MultiPartParser,
+        parsers.JSONParser,
+    )
+
+    renderer_classes = renderers.JSONRenderer
+
+    def post(self, req):
+        alpha = "hello"
+        return alpha
 
 
 @api_view(["POST"])
