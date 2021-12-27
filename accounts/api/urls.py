971c38c442e0a442e39b10abbd28b4eeec4fd593
Preston <preston@sudogami.com>
2021-12-26 19:54:32 -0500

    api-token-auth

--- accounts/api/urls.py
+++ accounts/api/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from rest_framework.authtoken import views
+
 from accounts.api.views import RegisterLookUp, registration_view
 
 app_name = "account"
@@ -6,4 +8,5 @@ app_name = "account"
 urlpatterns = [
     path("register", registration_view, name="register"),
     path("register/look", RegisterLookUp.as_view(), name="lookup"),
+    path("register/api-token-auth/", views.obtain_auth_token),
 ]
