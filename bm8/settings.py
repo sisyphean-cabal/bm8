a97275934229f8006021ca220cdfb978c23dcedd
Preston <preston@sudogami.com>
2021-12-27 03:02:37 -0500

    swagger UI added
    Cherry Pick swagger UI

--- bm8/settings.py
+++ bm8/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "corsheaders",
     "phonenumber_field",
+    "drf_yasg",
 ]
 
 MIDDLEWARE = [
@@ -111,9 +112,7 @@ DATABASES = {
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"
-    },
+    {"NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"},
     {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
     {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
     {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
--- bm8/urls.py
+++ bm8/urls.py
@@ -14,11 +14,32 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path, re_path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+# YASG related
+schema_v = get_schema_view(
+    openapi.Info(
+        title="Riff API",
+        default_version="0.0.1",
+        description="Endpoints for Armageddon",
+        terms_of_service="uwaht",
+        contact=openapi.Contact(email="Bryan@yiffriffrippers.org"),
+        license=openapi.License(name="BSD or some shit"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
     # REST
     path("api/account/", include("accounts.api.urls", "account_api")),
+    # YASG
+    re_path(r"^swagger(?P<format>\.json|\.yaml)$", schema_v.without_ui(cache_timeout=0), name="schema-json"),
+    re_path(r"^swagger/$", schema_v.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
+    re_path(r"^redoc/$", schema_v.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
 ]
