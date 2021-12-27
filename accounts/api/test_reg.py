7a7283be0ccdf724fee21553d600680f9132c905
Preston <preston@sudogami.com>
2021-12-26 19:46:00 -0500

    precomm changes

--- accounts/api/test_reg.py
+++ accounts/api/test_reg.py
@@ -1,19 +1,25 @@
+import json
+
 from django.test import TestCase
 from rest_framework.test import APIClient, APIRequestFactory
+
 from accounts.api.views import registration_view
-import json
 
 
 class RegisterTest(TestCase):
     client = APIClient()
     factory = APIRequestFactory()
 
-    def test_register(self):
+    def test_register_fail(self):
         data = {"password": "Lol124", "password_confirmation": "lol344"}
-        req = self.factory.post(
-            "/api/account/register", json.dumps(data), content_type="application/json"
-        )
+        req = self.factory.post("/api/account/register", json.dumps(data), content_type="application/json")
         view = registration_view
-        print(req.body)
         res = view(req)
         self.assertEqual(res.status_code, 400)
+
+    def test_register_success(self):
+        data = {"password": "Hacker123", "password_confirmation": "Hacker123"}
+        req = self.factory.post("/api/account/register", json.dumps(data), content_type="application/json")
+        view = registration_view
+        res = view(req)
+        self.assertEqual(res.status_code, 200)
