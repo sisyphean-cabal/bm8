8266ef48043fc1431f9576047d4fe59c82b8794f
Preston <preston@sudogami.com>
2021-12-27 07:48:49 -0500

    ObtainLoginToken, phone number field added

--- accounts/api/views.py
+++ accounts/api/views.py
@@ -1,9 +1,67 @@
-from rest_framework import generics
+from rest_framework import generics, parsers, renderers
+from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.compat import coreapi, coreschema
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.schemas import ManualSchema
+from rest_framework.schemas import coreapi as coreapi_schema
 
-from accounts.api.serializers import RegistrationSerializer, UserSerializer
+from accounts.api.serializers import LoginTokenSerializer, RegistrationSerializer, UserSerializer
 from accounts.models import User
+
+
+class ObtainLoginToken(ObtainAuthToken):
+    throttle_classes = ()
+    permission_classes = ()
+    parser_classes = (
+        parsers.FormParser,
+        parsers.MultiPartParser,
+        parsers.JSONParser,
+    )
+    renderer_classes = (renderers.JSONRenderer,)
+    serializer_class = LoginTokenSerializer
+
+    if coreapi_schema.is_enabled():
+        schema = ManualSchema(
+            fields=[
+                coreapi.Field(
+                    name="email",
+                    required=True,
+                    location="form",
+                    schema=coreschema.String(
+                        title="Email",
+                        description="Valid Email for authentication",
+                    ),
+                ),
+                coreapi.Field(
+                    name="phone_number",
+                    required=True,
+                    location="form",
+                    schema=coreschema.String(
+                        title="phone_number",
+                        description="Valid phone number for authentication",
+                    ),
+                ),
+                coreapi.Field(
+                    name="password",
+                    required=True,
+                    location="form",
+                    schema=coreschema.String(
+                        title="Password",
+                        description="Valid password for authentication",
+                    ),
+                ),
+            ],
+            encoding="application/json",
+        )
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data["user"]
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({"token": token.key})
 
 
 @api_view(["POST"])
@@ -16,8 +74,6 @@ def registration_view(req):
             data["response"] = "registration successful"
             data["email"] = account.email
             data["phone_number"] = str(account.phone_number)
-            # token = Token.objects.get(user=account).key
-            # data["token"] = token
         else:
             data = serializer.errors
         return Response(data)
