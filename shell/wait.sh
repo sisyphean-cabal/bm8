9e6fbed401adda1b38ecaf5e96fa10c7ea7cc9d1
dman <dman757@gmail.com>
2022-01-06 21:38:43 -0800

    nice

--- shell/wait.sh
+++ shell/wait.sh
@@ -10,6 +10,8 @@ until ./shell/wait-for-it.sh "$host"; do
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 1
 done
-  
+>&2 echo "Running migrations"
+python manage.py migrate
+
 >&2 echo "Postgres is up - executing command"
 exec "$@"
