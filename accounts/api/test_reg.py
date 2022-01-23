82b609294898cd15bf5673378d8ad74e89151f29
Preston <preston@sudogami.com>
2022-01-22 23:41:52 -0500

    test_reg_fail

--- accounts/api/test_reg.py
+++ accounts/api/test_reg.py
@@ -10,18 +10,23 @@ class RegisterTest(TestCase):
     client = APIClient()
     factory = APIRequestFactory()
 
-    # def test_register_fail(self):
-    #     data = {"password": "Lol124", "password_confirmation": "lol344"}
-    #     req = self.factory.post("/api/account/register", json.dumps(data), content_type="application/json")
-    #     view = registration_view
-    #     res = view(req)
-    #     self.assertEqual(res.status_code, 400)
+    def test_register_fail(self):
+        data = {"email": "", "phone": "", "password": "wordpass", "password_confirmation": "wordpizzle"}
+        req = self.factory.post("/api/account/register", json.dumps(data), content_type="application/json")
+        view = registration_view
+        res = view(req)
+
+        # why is this test returning 200?
+        self.assertEqual(res.status_code, 400)
 
     def test_register_success(self):
         data = {"email": "test@site.com", "phone": "", "password": "lol134", "password_confirmation": "lol134"}
         req = self.factory.post("/api/account/register", json.dumps(data), content_type="application/json")
         view = registration_view
         res = view(req)
+
+        # Test should be 200.
+
         self.assertEqual(res.status_code, 200)
 
     # def test_register_attempt(self):
